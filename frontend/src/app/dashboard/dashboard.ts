5135a3e676457eb1196e7d0ab8661967ea5ecb55
abdelmounaim sou <mounaim@mounaim.168.25.70>
2026-01-04 06:53:23 +0100

    feature/restructured the layout of the website added mao images

--- frontend/src/app/dashboard/dashboard.ts
+++ frontend/src/app/dashboard/dashboard.ts
@@ -1,34 +1,29 @@
 import { Component, inject, computed } from '@angular/core';
-import { CommonModule } from '@angular/common'; // Needed for Pipes (Date, Number)
+import { CommonModule } from '@angular/common';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { WeatherService } from '../services/weather';
-import { MeteoData } from '../models/weather.interface';
+import { HeaderComponent } from '../components/header/header';
+import { MapVisualizerComponent } from '../components/map-visualizer/map-visualizer';
+import { AlertListComponent } from '../components/alert-list/alert-list';
+import { ImageSliderComponent } from '../components/image-slider/image-slider';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './dashboard.html',
-  styleUrl: './dashboard.css' // We will use Tailwind classes in HTML, so this can stay empty
+  imports: [CommonModule, HeaderComponent, MapVisualizerComponent, AlertListComponent, ImageSliderComponent],
+  templateUrl: './dashboard.html'
 })
 export class DashboardComponent {
   private weatherService = inject(WeatherService);
-
-  // 1. Convert the Observable to a Read-Only Signal
-  // initialValue is required to prevent errors before data arrives
   forecastSignal = toSignal(this.weatherService.getForecast(), { initialValue: null });
 
-  // 2. Computed Signal: Extract only the data array for easier access
   weatherData = computed(() => {
     const response = this.forecastSignal();
     return response ? response.data : [];
   });
 
-  // 3. Computed Signal: Identify the most critical alert in the forecast
-  // This is part of the "Decision Support" aspect of your project
   criticalAlert = computed(() => {
     const data = this.weatherData();
-    // Find the first alert that is NOT null
     return data.find(day => day.alert !== null)?.alert || null;
   });
 }
